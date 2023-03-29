aeb9b19cdb89e1c0333023f89a709b368899ddaa
Michelle <michellebaker1129@gmail.com>
2023-03-29 12:55:47 -0700

    remove notes, keep only the work we like so far

---
+++ assets/style/script.js
@@ -0,0 +1,28 @@
+
+
+// build object for 1st prompt box
+var userEntry = "rock"
+
+var userChoice = prompt("rock paper or scissors?");
+var computerChoice = alert("your opponent chose" + )
+
+
+var gamePlay = {
+  userChoice: ["rock", "paper", "scissors"],
+  computerChoice: ["rock", "paper", "scissors"],
+  apptMessage: function () {
+    console.log("words about the game");
+  }
+};
+
+function youWon () {
+    console.log("Congrats! You WIN!")
+}
+
+function youLost () {
+    console.log("Darn it you lost. The computer played" + something + ".")
+}
+
+if (userChoice.includes(userEntry)) {
+    userChoice
+}
